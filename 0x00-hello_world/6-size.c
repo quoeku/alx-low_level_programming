11d8e79eee13febb8afc552ddc51c0fbc97ca065
quoeku <quoeku@gmail.com>
2022-04-10 07:05:49 -0700

    Task 6 corrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-
 /**
  *
- * Main -Entry point of code
+ * main -Entry point of code
  *
  * Return : Always 0 (success)
  */
 int main(void)
 {
-	printf("Size of a char: %eu byte(s)\n", sizeof(char));
-	printf("Size of an int: %eu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %eu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %eu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %eu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
