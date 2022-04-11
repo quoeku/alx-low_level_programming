d58345434db176203b9dd8cbce3ab3d0b60f8b37
quoeku <quoeku@gmail.com>
2022-04-10 19:22:40 -0700

    Print both upper and lowercase letters with putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point of code.
+ *
+ *Return: Always 0(Success)
+ **/
+
+int main(void)
+{
+	char  letter;
+
+	letter = 'a';
+	while(letter != '{')
+	{
+		putchar (letter);
+		letter++;
+	}
+	letter = 'A';
+	while (letter != '[')
+	{
+		putchar (letter);
+	}
+	putchar('\n');
+	return (0);
+}
