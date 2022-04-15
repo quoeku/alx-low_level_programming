b36b1c71bdcc29ca58a0da22b9741dd7b7dc836a
quoeku <quoeku@gmail.com>
2022-04-15 00:16:34 -0700

    Print base 16 letters and numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+
+}
